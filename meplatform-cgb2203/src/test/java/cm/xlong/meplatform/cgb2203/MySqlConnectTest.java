9292467f1c3be64d80fc7bbfb932cb860216094b
symbolfy <3526980767@qq.com>
2022-07-26 19:40:33 +0800

    初步基本搭建

---
+++ meplatform-cgb2203/src/test/java/cm/xlong/meplatform/cgb2203/MySqlConnectTest.java
@@ -0,0 +1,22 @@
+package cm.xlong.meplatform.cgb2203;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest
+@Slf4j
+public class MySqlConnectTest {
+    @Autowired
+    private DataSource dataSource;
+    @Test
+    public void testConnect() throws SQLException {
+        log.info("尝试连接");
+        dataSource.getConnection();
+        log.info("连接成功");
+    }
+}
